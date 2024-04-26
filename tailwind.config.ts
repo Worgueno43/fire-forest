f0339ad53201dbc9ce74b7bdf6d3d5c73bb1ea47
Yvan Thalamas <yvan@oktopod.io>
2024-04-26 12:12:56 +0200

    Adding color to match with fire / forest and ash

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,17 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["var(--font-sans)", ...fontFamily.sans]
       },
-    },
+      colors: {
+        current: "currentColor",
+        transparent: "transparent",
+        forest: "#18534F",
+        fire: "#E76F51",
+        main: "#E8D5CC",
+        ash: "#384454"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
