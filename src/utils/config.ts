fc02ec363144f5ee170cf0ded44c17c98ea7004b
Yvan Thalamas <yvan@oktopod.io>
2024-04-26 13:20:53 +0200

    adding default config set

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,8 +2,8 @@ import type { FirePosition, GridSize } from "./type";
 
 // Dimensions of the forest (hauteur > 0, largeur > 0)
 const grid: GridSize = {
-  hauteur: 3,
-  largeur: 3
+  hauteur: 10,
+  largeur: 10
 };
 
 // Default fire postion in the forrest /!\ (x >= 0, y >= 0)
@@ -13,12 +13,12 @@ const defaultFirePosition: FirePosition[] = [
     y: 1
   },
   {
-    x: 0,
-    y: 0
+    x: 8,
+    y: 7
   }
 ];
 
 // Probability of fire spread (0.0 to 1.0)
-const propagationProbability: number = 0.3;
+const propagationProbability: number = 0.5;
 
 export { grid, defaultFirePosition, propagationProbability };
