4d45fede2c74f62dd98ca5102e286d937c17539b
Yvan Thalamas <yvan@oktopod.io>
2024-04-26 12:58:14 +0200

    adding config unit test

---
+++ src/utils/config.test.ts
@@ -0,0 +1,39 @@
+import { defaultFirePosition, grid, propagationProbability } from "./config";
+
+describe("grid should be display", () => {
+  it("hauteur should be > 0", () => {
+    expect(grid.hauteur).toBeGreaterThan(0);
+  });
+
+  it("largeur should be > 0", () => {
+    expect(grid.largeur).toBeGreaterThan(0);
+  });
+});
+
+describe("propagationProbability should be beetween 0 and 1", () => {
+  it("propagationProbability should be >= 0", () => {
+    expect(propagationProbability).toBeGreaterThanOrEqual(0);
+  });
+
+  it("propagationProbability should be <= 1", () => {
+    expect(propagationProbability).toBeLessThanOrEqual(1);
+  });
+});
+
+describe("defaultFirePosition should be display", () => {
+  it("defaultFirePosition length should be >= 1", () => {
+    expect(defaultFirePosition.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it("defaultFirePosition x should be >= 0", () => {
+    defaultFirePosition.forEach(({ x }) => {
+      expect(x).toBeGreaterThanOrEqual(0);
+    });
+  });
+
+  it("defaultFirePosition y should be >= 0", () => {
+    defaultFirePosition.forEach(({ y }) => {
+      expect(y).toBeGreaterThanOrEqual(0);
+    });
+  });
+});
